fa4e8da77fa7674d227eeb06a337f85f635f8aae
aaron <aaron.lison@outlook.com>
2024-01-16 14:42:01 +0100

    use env vars for aikido api key and repo id

--- src/scripts/security_scan.sh
+++ src/scripts/security_scan.sh
@@ -3,12 +3,12 @@
 echo "Verifying parameters..."
 
 if [ -z "$AIKIDO_API_KEY" ]; then
-    echo "aikido_api_key not set. Please provide one and try again."
+    echo "AIKIDO_API_KEY not set. Please provide one as an environment variable and try again."
     exit 1
 fi
 
-if [ -z "$REPO_ID" ]; then
-    echo "repository_id not set. Please provide one and try again."
+if [ -z "$AIKIDO_REPOSITORY_ID" ]; then
+    echo "AIKIDO_REPOSITORY_ID not set. Please provide an environment variable one and try again."
     exit 1
 fi
 
@@ -24,4 +24,4 @@ fi
 
 echo "Starting Aikido CI scan..."
 
-aikido-api-client scan "$REPO_ID" "$BASE_COMMIT_SHA" "$HEAD_COMMIT_SHA" --apikey "$AIKIDO_API_KEY"
+aikido-api-client scan "$AIKIDO_REPOSITORY_ID" "$BASE_COMMIT_SHA" "$HEAD_COMMIT_SHA" --apikey "$AIKIDO_API_KEY"
