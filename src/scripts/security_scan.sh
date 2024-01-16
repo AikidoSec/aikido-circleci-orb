5efdd3d71bd19c2d3f89f570e9a28074a6ef6f44
aaron <aaron.lison@outlook.com>
2024-01-16 16:15:52 +0100

    dont use base branch or base commit

--- src/scripts/security_scan.sh
+++ src/scripts/security_scan.sh
@@ -12,20 +12,11 @@ if [ -z "$AIKIDO_REPOSITORY_ID" ]; then
     exit 1
 fi
 
-if [ -z "$BASE_BRANCH" ]; then
-    echo "base_branch not set. Please provide one and try again."
-    exit 1
-fi
-
 if [ -z "$HEAD_COMMIT_SHA" ]; then
     echo "head_commit_sha not set. Please provide one and try again."
     exit 1
 fi
 
-echo "Getting base commit id"
-
-BASE_COMMIT_ID=$(git merge-base origin/"$BASE_BRANCH" "$SHA_HEAD_COMMIT_ID")
-
 echo "Starting Aikido CI scan..."
 
-aikido-api-client scan "$AIKIDO_REPOSITORY_ID" "$BASE_COMMIT_ID" "$HEAD_COMMIT_SHA" --apikey "$AIKIDO_API_KEY"
+aikido-api-client scan "$AIKIDO_REPOSITORY_ID" "$HEAD_COMMIT_SHA" --apikey "$AIKIDO_API_KEY"
