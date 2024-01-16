26e114b45bcfe636b0746726bb2db8625d75ac51
aaron <aaron.lison@outlook.com>
2024-01-16 09:48:47 +0100

    create better steps for security scan job

---
+++ src/scripts/install.sh
@@ -0,0 +1,5 @@
+#!/bin/bash
+
+echo -n "Installing Aikido CI API client..."
+
+npm install -g @aikidosec/ci-api-client
--- src/scripts/security_scan.sh
+++ src/scripts/security_scan.sh
@@ -1,5 +1,27 @@
 #!/bin/bash
 
-CMD="aikido-api-client scan ${REPO_ID} ${BASE_COMMIT_SHA} ${HEAD_COMMIT_SHA} <<parameters.head_commit_sha>> --apikey ${AIKIDO_API_KEY}"
+echo -n "Verifying parameters..."
 
-eval "$CMD"
+if [ -z "$AIKIDO_API_KEY" ]; then
+    echo "aikido_api_key not set. Please provide one and try again."
+    exit 1
+fi
+
+if [ -z "$REPO_ID" ]; then
+    echo "repository_id not set. Please provide one and try again."
+    exit 1
+fi
+
+if [ -z "$BASE_COMMIT_SHA" ]; then
+    echo "base_commit_sha not set. Please provide one and try again."
+    exit 1
+fi
+
+if [ -z "$HEAD_COMMIT_SHA" ]; then
+    echo "head_commit_sha not set. Please provide one and try again."
+    exit 1
+fi
+
+echo -n "Starting Aikido CI scan..."
+
+aikido-api-client scan $REPO_ID $BASE_COMMIT_SHA $HEAD_COMMIT_SHA --apikey $AIKIDO_API_KEY
