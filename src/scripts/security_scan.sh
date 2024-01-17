5780eaeee510347ba7574910100fad18e3a0296c
aaron <aaron.lison@outlook.com>
2024-01-17 12:29:40 +0100

    echo fail on params

--- src/scripts/security_scan.sh
+++ src/scripts/security_scan.sh
@@ -29,14 +29,17 @@ AIKIDO_CMD="aikido-api-client scan $AIKIDO_REPOSITORY_ID $BASE_COMMIT_ID $CIRCLE
 
 # Additional configuration options
 
+echo "$FAIL_ON_DEPENDENCY_SCAN"
 if [ "$FAIL_ON_DEPENDENCY_SCAN" = "false" ]; then
     AIKIDO_CMD="$AIKIDO_CMD --no-fail-on-dependency-scan"
 fi
 
+echo "$FAIL_ON_SAST_SCAN"
 if [ "$FAIL_ON_SAST_SCAN" = "true" ]; then
     AIKIDO_CMD="$AIKIDO_CMD --fail-on-sast-scan"
 fi
 
+echo "$FAIL_ON_IAC_SCAN"
 if [ "$FAIL_ON_IAC_SCAN" = "true" ]; then
     AIKIDO_CMD="$AIKIDO_CMD --fail-on-iac-scan"
 fi
